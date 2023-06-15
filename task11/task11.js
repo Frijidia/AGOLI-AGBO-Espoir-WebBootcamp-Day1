a0bf61f18cf9a7bb1fa1f4f3dde6a3e1ceb31cb7
Frijidia <espoir.agoli-agbo@epitech.eu>
2023-06-15 15:24:26 +0100

    adding task11, task12, task13, task14, and task15

--- task11/task11.js
+++ task11/task11.js
@@ -1,5 +1,26 @@
-function sortByProperty(){
+function cName(a, b){
+    const name1 = a.name.toUpperCase();
+    const name2 = b.name.toUpperCase();
 
+    var comparison = 0;
+
+    if (name1 > name2) {
+        comparison = 1;
+    } else if (name1 < name2) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function cAge(a, b){
+    return a.age - b.age;
+}
+
+function sortByProperty(user, key){
+    if (key === "age")
+        return (user.sort(cAge))
+    if (key == "name")
+        return (user.sort(cName))
 }
 
 const persons = [
@@ -8,5 +29,5 @@ const persons = [
     { name: "Bob", age: 18 },
   ];
   
-  const sortedPersons = sortByProperty(persons, "age");
-  console.log("Sorted persons:", sortedPersons);
+//   const sortedPersons = sortByProperty(persons, "age");
+//   console.log("Sorted persons:", sortedPersons);
